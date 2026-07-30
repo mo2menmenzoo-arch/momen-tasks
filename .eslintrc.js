0f74a4a6ba27db8ecac244e6a08ab646b49e2593
mo2menmenzoo-arch <mo2menmenzoo@gmail.com>
2026-07-30 15:08:36 +0300

    fix: disable no-var-requires ESLint rule for optional deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-var-requires': 'off',
     'no-console': 'off',
   },
 };
